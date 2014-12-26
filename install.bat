b42ca32318ce115a108f6e8a522d48a5b3d73179
Takatoshi <email>
2014-12-27 00:29:14 +0900

    Add .vimrc.bundles and .vim to Windows installation. They can be loaded by vimrc now.

--- install.bat
+++ install.bat
@@ -4,3 +4,7 @@ set THIS_DIRECTORY=%~dp0
 
 if exist %USERPROFILE%\.vimrc del %USERPROFILE%\.vimrc
 mklink %USERPROFILE%\.vimrc %THIS_DIRECTORY%vimrc
+if exist %USERPROFILE%\.vimrc.bundles del %USERPROFILE%\.vimrc.bundles
+mklink %USERPROFILE%\.vimrc.bundles %THIS_DIRECTORY%vimrc.bundles
+if exist %USERPROFILE%\.vim del %USERPROFILE%\.vim
+mklink %USERPROFILE%\.vim %THIS_DIRECTORY%vim
