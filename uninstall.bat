ec8d03c13f3b423f343307a442af8c3c5d2f766d
Takatoshi <email>
2014-12-27 01:21:07 +0900

    Add .vimrc.bundles and .vim to uninstall.bat.

--- uninstall.bat
+++ uninstall.bat
@@ -1,3 +1,5 @@
 @echo off
 
 if exist %USERPROFILE%\.vimrc del %USERPROFILE%\.vimrc
+if exist %USERPROFILE%\.vimrc.bundles del %USERPROFILE%\.vimrc.bundles
+if exist %USERPROFILE%\.vim del %USERPROFILE%\.vim
