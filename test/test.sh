26659a8331fa3634bd23a9dd60e647679c6af6b5
iwataka <ap.gr.le@gmail.com>
2015-03-04 16:21:53 +0900

    Add tests about array and read command.

--- test/test.sh
+++ test/test.sh
@@ -29,6 +29,33 @@ dot_test() {
     fi
 }
 
+tail_test() {
+    local arr=($(tail_args 1 2 3))
+    if [[ ${arr[0]} == 2 ]]; then
+        echo 'tail_test successes'
+    else
+        echo 'tail_test fails'
+    fi
+}
+
+tail_args() {
+    local arr=$@
+    echo ${arr[@]:1}
+}
+
+read_test() {
+    printf "%s" "Wanna success in read_test? (yes/no) "
+    local res
+    read res
+    if [[ $res =~ (y|yes|Yes|YES) ]]; then
+        echo 'read_test successes'
+    else
+        echo 'read_test fails'
+    fi
+}
+
 regex_test
 echo_test
 dot_test
+tail_test
+read_test
