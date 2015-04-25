9e98cc031881528bc30c33903546831dc5bfcdcb
iwataka <ap.gr.le@gmail.com>
2015-04-25 23:11:25 +0900

    Move a function which installs default packages to install script.

--- install.sh
+++ install.sh
@@ -72,11 +72,46 @@ setup-symlinks() {
     ln -s $dotfiles_dir/vim ~/.nvim
 }
 
-if [[ ! -d $dfsdir ]]
-    git clone https://github.com/iwataka/dotfiles $dfsdir
-    cd $dfsdir
+install-packages() {
+    if [ $OSTYPE == "linux-gnu" ]; then
+        sudo apt-get update && sudo apt-get upgrade
+        # necessaary to clone various projects
+        sudo apt-get install git
+        # mainly clone openjdk
+        sudo apt-get install mercurial
+        # default shell
+        sudo apt-get install zsh
+        # default editor
+        sudo apt-get install vim
+        # cooperate with vim
+        sudo apt-get install tmux
+        # tagbar and so on
+        sudo apt-get install exuberant-ctags
+        # faster code-searching tool
+        sudo apt-get install silversearcher-ag
+        # displays directory structure as a tree
+        sudo apt-get install tree
+        # tweak images
+        sudo apt-get install imagemagick
+        # code formatter for java, c and cpp
+        sudo apt-get install astyle
+        # youcompleteme
+        sudo apt-get install build-essential cmake python-dev
+    fi
+}
+
+if [[ $# == 0 ]]; then
+    if [[ ! -d $dfsdir ]]; then
+        git clone https://github.com/iwataka/dotfiles $dfsdir
+        cd $dfsdir
+    fi
+    pre-setup
+    setup-bins
+    install-git-open
+    setup-symlink
+    install-packages
+else
+    for name in $@; do
+        $name
+    done
 fi
-pre-setup
-setup-bins
-install-git-open
-setup-symlink
--- script/install-packages.sh
+++ script/install-packages.sh
@@ -2,34 +2,6 @@
 
 set -e
 
-install-default-packages() {
-    if [ $OSTYPE == "linux-gnu" ]; then
-        sudo apt-get update && sudo apt-get upgrade
-        # necessaary to clone various projects
-        sudo apt-get install git
-        # mainly clone openjdk
-        sudo apt-get install mercurial
-        # default shell
-        sudo apt-get install zsh
-        # default editor
-        sudo apt-get install vim
-        # cooperate with vim
-        sudo apt-get install tmux
-        # tagbar and so on
-        sudo apt-get install exuberant-ctags
-        # faster code-searching tool
-        sudo apt-get install silversearcher-ag
-        # displays directory structure as a tree
-        sudo apt-get install tree
-        # tweak images
-        sudo apt-get install imagemagick
-        # code formatter for java, c and cpp
-        sudo apt-get install astyle
-        # youcompleteme
-        sudo apt-get install build-essential cmake python-dev
-    fi
-}
-
 open-command() {
     if [[ $OSTYPE == "linux-gnu" ]]; then
         echo "xdg-open"
