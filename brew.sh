6ece38cbde34563529ebf001b8618fabd6580896
iwataka <ap.gr.le@gmail.com>
2015-04-28 22:26:30 +0900

    Add scala and openjdk and remove some installations.

--- brew.sh
+++ brew.sh
@@ -35,42 +35,12 @@ if [ $OSTYPE == "linux-gnu" ]; then
     sudo apt-get install ant
     # used for install solarized colorscheme
     sudo apt-get install dconf-cli
+    # Scala
+    sudo apt-get install scala
+    sudo apt-get install sbt
+    # Java
+    sudo apt-get install openjdk-7-jdk
 fi
-
-open-command() {
-    if [[ $OSTYPE == "linux-gnu" ]]; then
-        echo "xdg-open"
-    elif [[ $OSTYPE == "darwin" ]]; then
-        echo "open"
-    elif [[ $OSTYPE == "cygwin" ]]; then
-        echo "cygstart"
-    else
-        echo "start"
-    fi
-}
-
-# oracle-java
-$(open-command) "http://www.oracle.com/technetwork/java/javase/downloads/index.html"
-
-# scala
-install-scala() {
-    local path=/usr/local/scala
-    if [[ -d $path ]]; then
-        cd $path
-        sudo git pull
-    else
-        sudo git clone https://github.com/scala/scala $path
-    fi
-}
-
-# sbt
-$(open-command) http://www.scala-sbt.org/download.html
-
-# go
-$(open-command) "https://golang.org/doc/install"
-
-# idea
-$(open-command) https://www.jetbrains.com/idea/download/
 
 # rbenv
 install-rbenv() {
@@ -104,16 +74,6 @@ install-antigen() {
     fi
 }
 
-install-neovim() {
-    local path=$HOME/projects/neovim
-    if [[ -d $path ]]; then
-        cd $path
-        git pull
-    else
-        git clone https://github.com/neovim/neovim $path
-    fi
-}
-
 # gnome-terminal-colors-solarized
 install-gnome-terminal-colors-solarized() {
     local name=gnome-terminal-colors-solarized
@@ -140,25 +100,6 @@ install-poweline-fonts() {
     fi
 }
 
-install-git-open() {
-    local path=$HOME/projects/git-open
-    if [[ ! -d $path ]]; then
-        git clone https://github.com/paulirish/git-open $path
-    else
-        cd $path
-        git pull
-    fi
-    # Remove old git-open if exists
-    if [ -e ~/bin/git-open ]; then
-        rm ~/bin/git-open
-    fi
-    # Copy git-open file and make it executable.
-    cp $path/git-open ~/bin/git-open
-    chmod u+x ~/bin/git-open
-}
-
 install-antigen
-install-neovim
 install-gnome-terminal-colors-solarized
 install-poweline-fonts
-install-git-open
