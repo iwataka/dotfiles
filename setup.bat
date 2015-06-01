6af60b1d374c3a25bb8656862f890d824d37eae8
iwataka <ap.gr.le@gmail.com>
2015-06-01 17:52:04 +0900

    Add some new targets to setup.bat

--- setup.bat
+++ setup.bat
@@ -10,3 +10,15 @@ mklink /J /D %USERPROFILE%\.vim %DOTFILES%vim
 
 if exist %USERPROFILE%\.gitconfig del %USERPROFILE%\.gitconfig
 mklink /H %USERPROFILE%\.gitconfig %DOTFILES%gitconfig
+
+if exist %USERPROFILE%\.agignore del %USERPROFILE%\.agignore
+mklink /H %USERPROFILE%\.agignore %DOTFILES%agignore
+
+if exist %USERPROFILE%\.sbtrc del %USERPROFILE%\.sbtrc
+mklink /H %USERPROFILE%\.sbtrc %DOTFILES%sbtrc
+
+if exist %USERPROFILE%\.vimperatorrc del %USERPROFILE%\.vimperatorrc
+mklink /H %USERPROFILE%\.vimperatorrc %DOTFILES%vimperatorrc
+
+if exist %USERPROFILE%\.ctags del %USERPROFILE%\.ctags
+mklink /H %USERPROFILE%\.ctags %DOTFILES%ctags
