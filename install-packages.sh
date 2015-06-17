9d767db471ab01d45779d240cbc2cf806fc6d629
iwataka <ap.gr.le@gmail.com>
2015-06-18 01:20:04 +0900

    Remove java installation.

--- install-packages.sh
+++ install-packages.sh
@@ -24,24 +24,11 @@ git-clone-if-not-exists() {
     fi
 }
 
-# Usage: install-oracle-java $version
-# Following command must be run before executing this function.
-# sudo add-apt-repository [-y] ppa:webupd8team/java
-install-oracle-java() {
-    sudo apt-get install -y oracle-java${1}-installer
-    cd /usr/lib/jvm/java-${1}-oracle
-    if [[ -e src.zip && ! -d src ]]; then
-        sudo unzip -d src src.zip
-    fi
-}
-
 if [ $OSTYPE == "linux-gnu" ]; then
     # update apt-get itself
     sudo apt-get update
     # upgrade packages
     sudo apt-get upgrade
-    # install apt-add-repository command
-    sudo apt-get install -y python-software-properties
     # xdg-open and so on
     sudo apt-get install -y xdg-utils
     # necessaary to clone various projects
@@ -76,14 +63,6 @@ if [ $OSTYPE == "linux-gnu" ]; then
     sudo npm -g install instant-markdown-d
     # Chrome
     sudo apt-get install google-chrome-stable
-    # Java
-    # If you want to switch other jdk, run this command:
-    # sudo update-alternatives --config java
-    # openjdk is currently unused
-    # sudo apt-get install -y openjdk-7-jdk
-    sudo add-apt-repository -y ppa:webupd8team/java
-    sudo apt-get update
-    install-oracle-java 8
 elif [[ $OSTYPE == "darwin"* ]]; then
     # Some sentences derived from https://github.com/mathiasbynens/dotfiles
     brew update
