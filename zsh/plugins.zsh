debcfd72ece7bf099c0b7b499046f8dd3d82d5da
iwataka <ap.gr.le@gmail.com>
2015-07-15 20:38:23 +0900

    Add pure prompt.

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -2,4 +2,7 @@ if [[ -d $HOME/.antigen ]]; then
   source $HOME/.antigen/antigen.zsh
   antigen bundle rupa/z
   antigen bundle zsh-users/zsh-syntax-highlighting
+  antigen bundle
+  antigen bundle mafredri/zsh-async
+  antigen bundle sindresorhus/pure
 fi
