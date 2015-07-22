b4ee319aa5dd6742f2918095cb57b64bd463f43f
iwataka <ap.gr.le@gmail.com>
2015-07-22 19:33:33 +0900

    Fix plugin.zsh.

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -2,9 +2,9 @@ if [[ -d $HOME/.antigen ]]; then
   source $HOME/.antigen/antigen.zsh
   antigen bundle rupa/z
   antigen bundle zsh-users/zsh-syntax-highlighting
-  antigen bundle
   antigen bundle mafredri/zsh-async
   antigen bundle sindresorhus/pure
+  antigen apply
 else
   source $HOME/.zsh/prompt.zsh
 fi
