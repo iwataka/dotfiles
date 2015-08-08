cc9737a5a88f08540d20abc581b0b7f9b609bb62
iwataka <ap.gr.le@gmail.com>
2015-08-09 01:45:36 +0900

    Revert "Replace some aliases with git plugin in oh-my-zsh."
    This reverts commit da616d5df721ab4d1dfd57f920d08b96ab994d8b.

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,6 +1,5 @@
 if [[ -d $HOME/.antigen ]]; then
   source $HOME/.antigen/antigen.zsh
-  antigen bundle git
   antigen bundle rupa/z
   antigen bundle zsh-users/zsh-syntax-highlighting
   antigen bundle mafredri/zsh-async
