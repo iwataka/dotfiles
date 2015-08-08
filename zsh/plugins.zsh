856b58c94d20d427f4f8542c78311b1f42074192
iwataka <ap.gr.le@gmail.com>
2015-08-09 01:53:11 +0900

    Add git plugin of oh-my-zsh.

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,5 +1,6 @@
 if [[ -d $HOME/.antigen ]]; then
   source $HOME/.antigen/antigen.zsh
+  antigen bundle git
   antigen bundle rupa/z
   antigen bundle zsh-users/zsh-syntax-highlighting
   antigen bundle mafredri/zsh-async
@@ -7,4 +8,5 @@ if [[ -d $HOME/.antigen ]]; then
   antigen apply
 else
   source $HOME/.zsh/prompt.zsh
+  source $HOME/.zsh/git.zsh
 fi
