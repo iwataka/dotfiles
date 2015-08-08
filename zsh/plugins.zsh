da616d5df721ab4d1dfd57f920d08b96ab994d8b
iwataka <ap.gr.le@gmail.com>
2015-08-08 16:44:07 +0900

    Replace some aliases with git plugin in oh-my-zsh.

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,5 +1,6 @@
 if [[ -d $HOME/.antigen ]]; then
   source $HOME/.antigen/antigen.zsh
+  antigen bundle git
   antigen bundle rupa/z
   antigen bundle zsh-users/zsh-syntax-highlighting
   antigen bundle mafredri/zsh-async
