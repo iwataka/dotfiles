a4a6a4839f35555ab51a38f63cbf168393168903
iwataka <ap.gr.le@gmail.com>
2015-09-15 14:45:42 +0900

    Add more functionalities to lazy installers.
    Functionalities such as installing requirements.

--- bin/pwd.sh
+++ bin/pwd.sh
@@ -1,8 +1,30 @@
 #!/usr/bin/env bash
 
+set -e
+
+install_requirements() {
+    local platform=$(uname -s)
+    if [[ $platform == "Linux" ]]
+    then
+        apt-get --version &> /dev/null
+        local apt_get_found=$(("$?" == 0))
+        if $apt_get_found
+        then
+            sudo apt-get install gnupg
+        fi
+    fi
+}
+
 if [[ ! -d ~/lib/pwd.sh ]]
 then
+    install_requirements
     git clone https://github.com/drduh/pwd.sh ~/lib/pwd.sh
+elif [[ "$1" == "--upgrade" ]]
+then
+    cd ~/lib/pwd.sh
+    git pull origin master
+else
+    # ~/lib/pwd.sh/pwd.sh "$@"
+    echo "$0"
 fi
 
-~/lib/pwd.sh/pwd.sh "$@"
