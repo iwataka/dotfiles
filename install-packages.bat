d20862c5b11ec265959b6ca58239d85cf1217e89
iwataka <ap.gr.le@gmail.com>
2015-12-06 03:38:10 +0900

    Add some more packages to install-package.bat.

--- install-packages.bat
+++ install-packages.bat
@@ -1,7 +1,17 @@
 choco install git
 choco install imagemagick
 choco install pandoc
+choco install gpg4win
+
+rem notepad++ is now disabled
+rem choco install notepad++
+
+rem An editor for environment variables
 choco install devbox-rapidee
-choco install notepad++
-choco install launchy
-choco install sharpkeys
+
+choco install golang
+rem the command for Google Drive written in Go.
+rem go get -u github.com/odeke-em/drive/cmd/drive
+
+rem If you want to replace key with other key, install this
+rem choco install sharpkeys
