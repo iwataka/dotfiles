a14f3c7cabb7812d465eb8a9f2ed8b36cd8a94f7
iwataka <ap.gr.le@gmail.com>
2016-03-15 06:10:20 +0900

    Setup antigen automatically.

--- zsh/plugins.main.zsh
+++ zsh/plugins.main.zsh
@@ -1,25 +1,29 @@
-if [[ -d $HOME/.antigen ]]; then
-  source $HOME/.antigen/antigen.zsh
-  antigen bundle docker
-  antigen bundle extract
-  antigen bundle git
-  antigen bundle github
-  antigen bundle gitignore
-  export ZSH_TMUX_AUTOSTART=true
-  antigen bundle tmux
-  antigen bundle rupa/z
-  antigen bundle zsh-users/zsh-syntax-highlighting
-  antigen bundle mafredri/zsh-async
-  antigen bundle sindresorhus/pure
-  antigen apply
-else
-  for _sub_plugin in $HOME/.zsh/*.sub.zsh
-  do
-    source $_sub_plugin
-  done
-  # Load the common shell configurations if unable to use antigen.
-  for _sh_file in $HOME/.sh/*.sh
-  do
-    source $_sh_file
-  done
+if [[ ! -d $HOME/.antigen ]]; then
+  git clone https://github.com/zsh-users/antigen.git ~/.antigen
 fi
+
+source $HOME/.antigen/antigen.zsh
+antigen bundle docker
+antigen bundle extract
+antigen bundle git
+antigen bundle github
+antigen bundle gitignore
+antigen bundle rupa/z
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle mafredri/zsh-async
+antigen bundle sindresorhus/pure
+antigen apply
+# export ZSH_TMUX_AUTOSTART=true
+# antigen bundle tmux
+
+# else
+#   for _sub_plugin in $HOME/.zsh/*.sub.zsh
+#   do
+#     source $_sub_plugin
+#   done
+#   # Load the common shell configurations if unable to use antigen.
+#   for _sh_file in $HOME/.sh/*.sh
+#   do
+#     source $_sh_file
+#   done
+# fi
