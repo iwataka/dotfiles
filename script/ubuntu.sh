0991e559153ac85f3da7d88d9177ba9551a6b29b
iwataka <ap.gr.le@gmail.com>
2016-04-29 16:16:12 +0900

    Fix ubuntu configuration script

--- script/ubuntu.sh
+++ script/ubuntu.sh
@@ -3,12 +3,14 @@
 set -e
 
 # Enable to reboot after 30 seconds when panic occured.
+# See http://askubuntu.com/questions/43555/how-to-configure-automatic-reboot-after-kernel-panic
 if [[ -z $(grep kernel.panic /etc/sysctl.conf) ]]; then
     echo "kernel.panic = 30" |sudo tee -a /etc/sysctl.conf
     sysctl -p
 fi
 
 # Deactivate bluetooth on system startup
-if [[ -z $(grep InitiallyPowered /etc/bluetooth/main.conf) ]]; then
-    echo "InitiallyPowered = false" |sudo tee -a /etc/bluetooth/main.conf
+# See http://askubuntu.com/questions/67758/how-can-i-deactivate-bluetooth-on-system-startup
+if [[ -z $(grep "rfkill block bluetooth" /etc/rc.local) ]]; then
+    echo "rfkill block bluetooth" |sudo tee -a /etc/rc.local
 fi
