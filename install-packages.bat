b95c1df1d2e05230bfd0dad3391528d657a9d9fd
iwataka <ap.gr.le@gmail.com>
2018-04-23 10:56:09 +0900

    Modify install-packages.bat to suppress input prompt

--- install-packages.bat
+++ install-packages.bat
@@ -1,7 +1,7 @@
-choco install git
-choco install imagemagick
-choco install pandoc
-choco install gpg4win
+choco install -y git
+choco install -y imagemagick
+choco install -y pandoc
+choco install -y gpg4win
 
 rem An editor for environment variables
-choco install devbox-rapidee
+choco install -y devbox-rapidee
