34b5cff388e2d666d37bf1fa9521d8ebf9244277
iwataka <ap.gr.le@gmail.com>
2019-06-06 03:24:56 +0900

    use neovim when available

--- zsh/vim.zsh
+++ zsh/vim.zsh
@@ -1,17 +1,12 @@
 vim() {
-    if [[ $USE_NEOVIM_INSTEAD_OF_VIM == true ]]
+    if ! (( $+_has_working_nvim ))
     then
-        if ! (( $+_has_working_nvim ))
-        then
-            nvim --version &> /dev/null
-            _has_working_nvim=$(($? == 0))
-        fi
-        if (( $_has_working_nvim ))
-        then
-            nvim "$@"
-        else
-            command vim "$@"
-        fi
+        nvim --version &> /dev/null
+        _has_working_nvim=$(($? == 0))
+    fi
+    if (( $_has_working_nvim ))
+    then
+        nvim "$@"
     else
         command vim "$@"
     fi
