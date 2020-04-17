600105c6b30f92276627c2439eff569b07e55143
iwataka <ap.gr.le@gmail.com>
2020-04-17 19:52:02 +0900

    update ghq script

--- zsh/ghq.zsh
+++ zsh/ghq.zsh
@@ -1,69 +1,48 @@
-# TEMPORARILY REPLACE THIS SCRIPT WITH https://github.com/x-motemen/ghq/blob/master/misc/zsh/_ghq
+function ghq-cd() {
+    filters=(peco fzf)
+    for filter in ${filters}; do
+        command -v ${filter} >& /dev/null
+        if [ $? -eq 0 ]; then
+            local repo_path="$(ghq list -p | ${filter})"
+            if [ -n "${repo_path}" ]; then
+                cd ${repo_path}
+            fi
+            return 0
+        fi
+    done
+}
 
-# ghq() {
-#     command -v ghq >& /dev/null
-#     if [ $? -eq 0 ]; then
-#         if [ "$1" = "cd" ]; then
-#             ghq-cd "$@[2,-1]"
-#         elif [ "$1" = "create" ]; then
-#             ghq-create "$@[2,-1]"
-#         else
-#             command ghq "$@"
-#         fi
-#     fi
-# }
+function ghq-import() {
+    origin_url=$(git config remote.origin.url)
+    if [ ! $? -eq 0 ]; then
+        echo "you are not in Git repository or remote.origin.url is not set" >&2
+        return 1
+    fi
+    rel_path="$(echo ${origin_url} |awk -F'://' '{print $2}')"
+    rel_path_trimmed=$(dirname ${rel_path})/${$(basename ${rel_path})%.git}
 
-# # This function can't be implemented in Go because Go can't change the current
-# # directory of the main process.
-# ghq-cd() {
-#     command -v peco >& /dev/null
-#     if [ $? -eq 0 -a "$#" -eq 0 ]; then
-#         local repo_path="$(ghq list -p | peco)"
-#         if [ -n "${repo_path}" ]; then
-#             cd ${repo_path}
-#         fi
-#     else
-#         local repo_path="$(ghq list -p |grep /$1\$)"
-#         if [ -n "${repo_path}" ]; then
-#             cd ${repo_path}
-#         else
-#             ghq_create "$@"
-#         fi
-#     fi
-# }
+    ghq_root="$(ghq root)"
+    src="$(git rev-parse --show-toplevel)"
+    dest="${ghq_root%/}/${rel_path_trimmed}"
+    if [ ${src} == ${dest} ]; then
+        echo "already imported" >&2
+        return 1
+    fi
+    if [ -d "${dest}" ]; then
+        echo "unable to import because ${dest} already exists" >&2
+        return 1
+    fi
 
-# ghq-create() {
-#     local user="$(git config github.user)"
-#     local root="$(ghq root)"
-#     if [ -n "${user}" ]; then
-#         local project="${root}/github.com/${user}/$1"
-#         mkdir ${project}
-#         cd ${project}
-#         git init
-#     fi
-# }
+    echo "move ${src} -> ${dest}"
+    if read -q "?continue?(y|n)"; then
+        echo
+    else
+        echo
+        echo abort >&2
+        return 1
+    fi
 
-# if [[ ! -o interactive ]]; then
-#     return
-# fi
-
-# # compctl -K _ghq ghq
-
-# _ghq() {
-#     # local words completions
-#     local cmds
-#     # read -cA words
-
-#     # if [ "${#words}" -eq 2 ]; then
-#     if (( CURRENT == 2 )); then
-#         # completions=$(echo "get\nlist\nlook\nimport\nroot\ncd\ncreate\nhelp\nh\n--version\n-v\n-h")
-#         cmds=($(echo "get\nlist\nlook\nimport\nroot\ncd\ncreate\nhelp\nh\n--version\n-v\n-h"))
-#         _describe -t commands "sub commands" cmds
-#     else
-#         # if [ "${words[2,-2]}" = "look" ] || [ "${words[2,-2]}" = "cd" ]; then
-#         #     completions=$(ghq list --unique)
-#         # fi
-#     fi
-
-#     # reply=("${(ps:\n:)completions}")
-# }
+    cd $(dirname ${src})
+    mkdir -p $(dirname ${dest})
+    mv ${src} ${dest}
+}
