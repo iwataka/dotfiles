3bbccf5fb3c25f71191b8d063c2dcac1e2a30d38
iwataka <ap.gr.le@gmail.com>
2021-12-10 02:16:05 +0900

    Format zsh/plugins.zsh

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -21,10 +21,10 @@ fi
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
-    printf "Install? [y/N]: "
-    if read -q; then
-        echo; zplug install
-    fi
+  printf "Install? [y/N]: "
+  if read -q; then
+    echo; zplug install
+  fi
 fi
 
 # Then, source plugins and add commands to $PATH
