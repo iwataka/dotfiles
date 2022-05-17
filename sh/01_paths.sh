e6f3a65a38daad004b1f35f1d4a7233eb05b6c61
iwataka <ap.gr.le@gmail.com>
2022-05-18 04:21:56 +0900

    modify brew PATH

--- sh/01_paths.sh
+++ sh/01_paths.sh
@@ -5,12 +5,19 @@ test -d ~/bin && export PATH=~/bin:"$PATH"
 
 while read -r cmd; do
     if [ "$(uname -s)" = "Darwin" ] && brew --prefix "$cmd" >/dev/null 2>&1; then
-        p="$(brew --prefix "$cmd")/libexec/gnubin"
+        if [ "$cmd" = "gawk" ]
+        then
+            p="$(brew --prefix "$cmd")/bin"
+        else
+            p="$(brew --prefix "$cmd")/libexec/gnubin"
+        fi
         export PATH="$p:$PATH"
     fi
 done <<EOF
 coreutils
 findutils
-gsed
+gnu-sed
 grep
+gnu-tar
+gawk
 EOF
