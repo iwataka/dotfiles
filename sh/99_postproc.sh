a47b672556577d725ae58d65f1b0de0993ed5563
iwataka <ap.gr.le@gmail.com>
2022-06-05 18:56:46 +0900

    Use nvim instead of vim for VISUAL and EDITOR if available

--- sh/99_postproc.sh
+++ sh/99_postproc.sh
@@ -5,7 +5,10 @@ command -v xdg-open >/dev/null && alias open='xdg-open'
 command -v wsl-open >/dev/null && alias open='wsl-open'
 command -v cygstart >/dev/null && alias open='cygstart'
 
-command -v nvim >/dev/null && alias vim='nvim'
+command -v nvim >/dev/null && \
+    alias vim='nvim' && \
+    export VISUAL=nvim && \
+    export EDITOR=$VISUAL
 
 command -v exa >/dev/null && alias ls='exa'
 
