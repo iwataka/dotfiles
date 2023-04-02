76923052daac94391574dfe8d1167bfc1bf5beca
iwataka <ap.gr.le@gmail.com>
2023-04-02 20:36:44 +0900

    Add fish functions imitating Zsh's auto_resume

--- fish/config.fish
+++ fish/config.fish
@@ -39,3 +39,41 @@ if test -z "$ASDF_DIR"
 end
 starship init fish |source
 zoxide init fish |source
+
+# This imitates Zsh's auto_resume feature.
+# If the given command name matches the background job, bring it to foreground
+# instead.
+function _auto_resume_preexec --on-event fish_preexec
+    if string match -rq '\s' "$argv"
+        return 0
+    end
+    set -g _auto_resume_jobid (jobs |
+            tail -n -1 |  # delete the first header line
+            awk '$5 == "'$argv'"' |  # extract jobs whose command equals to the given one
+            head -n 1 |
+            awk '{ print $2 }' |  # print Group ID
+            string trim)
+    if test -n "$_auto_resume_jobid"
+        if functions -q "$argv"
+            functions -c "$argv" _auto_resume_"$argv"
+        end
+        function "$argv"
+            fg "$_auto_resume_jobid"
+        end
+    end
+end
+
+# Clear some functions and variables craeted by _auto_resume_preexec.
+function _auto_resume_postexec --on-event fish_postexec
+    if string match -rq '\s' "$argv"
+        return 0
+    end
+    if test -n "$_auto_resume_jobid"
+        functions -e "$argv"
+        if functions -q _auto_resume_"$argv"
+            functions -c _auto_resume_"$argv" "$argv"
+            functions -e _auto_resume_"$argv"
+        end
+        set -e _auto_resume_jobid
+    end
+end
